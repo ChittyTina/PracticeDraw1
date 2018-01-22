0a0bfd26ee373492dcd661f5d7c815ed10d6b6fc
chitty <chittychenting@126.com>
2018-01-22 17:45:51 +0800

    practice 8 9 OK

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,22 +2,35 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice10HistogramView extends View {
 
+    private Paint paint;
+    private Path path;
+
     public Practice10HistogramView(Context context) {
         super(context);
+        init();
     }
 
     public Practice10HistogramView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice10HistogramView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        path = new Path();
     }
 
     @Override
@@ -26,5 +39,10 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
+//        paint.setColor();
+//        canvas.drawPoint(100,20,paint);
+
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice9DrawPathView.java
@@ -1,23 +1,45 @@
 package com.hencoder.hencoderpracticedraw1.practice;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.View;
 
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class Practice9DrawPathView extends View {
+
+    private Paint paint;
+    private Path path;
 
     public Practice9DrawPathView(Context context) {
         super(context);
+        init();
     }
 
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice9DrawPathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        path = new Path();
+
+        path.addArc(400, 200, 550, 350, -220, 220);
+        path.arcTo(550, 200, 700, 350, -180, 220, false);
+        path.lineTo(550, 462);
     }
 
     @Override
@@ -25,5 +47,8 @@ public class Practice9DrawPathView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawPath() 方法画心形
+
+        canvas.drawPath(path,paint);
+
     }
 }
